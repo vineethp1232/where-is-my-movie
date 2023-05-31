2c930ad21ee9b4339b19728b0a320e13f47555be
vineeth <vineethp1232@gmail.com>
2023-06-01 01:20:27 +0530

    watch button added

--- src/movieDetails.js
+++ src/movieDetails.js
@@ -12,6 +12,7 @@ function MovieDetails({movies,getWatchList,watchlist,isFromWatchlist}) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }, [id]);
     
+
   return (
     <div className='top' >
       <div className="movie-card"  >
@@ -23,27 +24,54 @@ function MovieDetails({movies,getWatchList,watchlist,isFromWatchlist}) {
     <div className="hero"  style={{backgroundImage:`url(${myMovie.backdropURLs.original})`}}>
             
       <div className="details" >
-      
+      <div>
         <div className="title1">{myMovie.title}</div>
 
         <div className="title2">{myMovie.tagline}</div>
         <h3 className='year'>{myMovie.year}, {myMovie.directors[0]}</h3>
         <span className="rating">IMDB rating {myMovie.imdbRating}/100</span>  
         <p>{myMovie.genres.map(genre=>{return (<span style={{paddingRight:"1vw"}} className='genre'>{genre.name}</span>)})}</p>
- 
+        </div>
+        {myMovie.streamingInfo.in && Object.keys(myMovie.streamingInfo.in).length > 0 ? (
+  <div>
+  
+    {myMovie.streamingInfo.in.hotstar && (
+      <a href={myMovie.streamingInfo.in.hotstar[0].link} target="_blank" rel="noopener noreferrer">
+        <button className="button2" id="watchNow">Watch on Hotstar</button>
+      </a>
+    )}
+    {myMovie.streamingInfo.in.prime && (
+      <a href={myMovie.streamingInfo.in.prime[0].link} target="_blank" rel="noopener noreferrer">
+        <button className="button2" id="watchNow">Watch on prime video</button>
+      </a>
+    )}
+      {myMovie.streamingInfo.in.netflix && (
+      <a href={myMovie.streamingInfo.in.netflix[0].link} target="_blank" rel="noopener noreferrer">
+        <button className="button2" id="watchNow">Watch on Netflix</button>
+      </a>
+    )}
+    {myMovie.streamingInfo.in.apple && (
+      <a href={myMovie.streamingInfo.in.apple[0].link} target="_blank" rel="noopener noreferrer">
+        <button className="button2" id="watchNow">Watch on Apple</button>
+      </a>
+    )}
+    {/* Render other platforms in a similar way */}
+  </div>
+) : (
+  <p></p>
+)}
+
         </div></div>
         <div className='description'>
           <div className='column1'>
-        {myMovie.youtubeTrailerVideoId ?<iframe  src={`https://www.youtube.com/embed/${myMovie.youtubeTrailerVideoId}`} frameBorder="0" allowFullscreen title={myMovie.title}></iframe>:<img className="iframe" src={myMovie.backdropURLs.original} alt={myMovie.title}/>}
+        {myMovie.youtubeTrailerVideoId ?<iframe  src={`https://www.youtube.com/embed/${myMovie.youtubeTrailerVideoId}`} frameBorder="0" allowFullScreen title={myMovie.title}></iframe>:<img className="iframe" src={myMovie.backdropURLs.original} alt={myMovie.title}/>}
 
         </div>
         <div className='column2'>
           <p>{myMovie.overview}</p>
            <div className='cast'>cast: {myMovie.cast.map(item=>{return <span>{item} | </span>})}</div>
            <div className='buttons'>
-          {/* { myMovie.streamingInfo.in.hotstar && <Link to={myMovie.streamingInfo.in.hotstar.watchLink} target="_blank">
-           <button className='button2'>Watch Now</button>
-           </Link>} */}
+
            <button className='button2' onClick={()=>getWatchList(myMovie,isFromWatchlist)}>{watchlist.some(element=>element.imdbId===myMovie.imdbId)?"Remove from Watchlist":"Add To Watchlist"}</button>
            
            </div>
